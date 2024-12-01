0982ef99c52a7674b355cb5ed178d8a463a0b37b
podestal <luisrodriguezpodesta@gmail.com>
2024-12-01 11:26:57 -0500

    school models

---
+++ app/school/serializers.py
@@ -0,0 +1,38 @@
+# from . import models
+# from rest_framework import serializers
+
+# class AreaSerializer(serializers.ModelSerializer):
+#     model = models.Area
+#     fields = '__all__'
+
+# class SchoolSerializer(serializers.ModelSerializer):
+#     model = models.School
+#     fields = '__all__'
+
+# class CompetenceSerializer(serializers.ModelSerializer):
+#     model = models.Competence
+#     fields = '__all__'
+
+# class CapacitySerializer(serializers.ModelSerializer):
+#     model = models.Capacity
+#     fields = '__all__'
+
+# class ClaseSerializer(serializers.ModelSerializer):
+#     model = models.Clase
+#     fields = '__all__'
+
+# class InstructorSerializer(serializers.ModelSerializer):
+#     model = models.Instructor
+#     fields = '__all__'
+
+# class StudentSerializer(serializers.ModelSerializer):
+#     model = models.Student
+#     fields = '__all__'
+
+# class TutorSerializer(serializers.ModelSerializer):
+#     model = models.Tutor
+#     fields = '__all__'
+
+# class TutorSerializer(serializers.ModelSerializer):
+#     model = models.Tutor
+#     fields = '__all__'

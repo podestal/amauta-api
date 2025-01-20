6c13869393739ebdaef4094869cbe496eb3e937b
podestal <luisrodriguezpodesta@gmail.com>
2025-01-20 07:04:21 -0500

    get by classroom attendances tests

--- app/school/tests/test_attendance.py
+++ app/school/tests/test_attendance.py
@@ -1,7 +1,8 @@
 import pytest
+from datetime import date
 from rest_framework import status
 from model_bakery import baker
-from school.models import Atendance, Student
+from school.models import Atendance, Student, Clase
 
 @pytest.fixture
 def create_student():
@@ -18,6 +19,14 @@ def create_atendance(create_student):
                 observations='Observations', 
                 attendance_type='A',
                 kind='I')
+
+@pytest.fixture
+def create_clase():
+    """Fixture to create a category for the authenticated user."""
+    grade = '1'
+    level = 'S'
+    section = 'A'
+    return baker.make(Clase, grade=grade, level=level, section=section)
 
 @pytest.mark.django_db
 class TestAtendance:
@@ -77,6 +86,49 @@ class TestAtendance:
         assert response.json()[0]['status'] == 'O'
         assert response.json()[0]['kind'] == 'I'
         assert response.json()[0]['created_by'] == 'John Doe'
+
+    def test_by_classroom_without_query_param_return_400(self, create_authenticate_user):
+        """
+        Test the byClassroom method when the 'classroom' query parameter is missing.
+        """
+        response = create_authenticate_user.get("/api/atendance/byClassroom/")
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json() == {"error": "Classroom parameter is required"}
+
+    def test_by_classroom_with_valid_data(self, create_authenticate_user, create_clase):
+        """
+        Test the byClassroom method with valid classroom and today's date.
+        """
+
+        student1 = baker.make(Student, uid='46345643', first_name='Tom', last_name='Doe', tutor_phone='123456789', clase=create_clase)
+        student2 = baker.make(Student, uid='46345644', first_name='John', last_name='Doe', tutor_phone='123456789', clase=create_clase)
+        other_clase = baker.make(Clase, grade='3', level='S', section='A')
+        other_student = baker.make(Student, uid='46345645', first_name='Jane', last_name='Doe', tutor_phone='123456789', clase=other_clase)
+        attendance1 = baker.make(Atendance, student=student1, status='O', kind='I', attendance_type='A', created_by='Manuel Doe')
+        attendance2 = baker.make(Atendance, student=student2, status='O', kind='I', attendance_type='A', created_by='Manuel Doe')
+        baker.make(Atendance, student=other_student, status='O', kind='I', attendance_type='A', created_by='Manuel Doe') 
+
+        response = create_authenticate_user.get(f"/api/atendance/byClassroom/?classroom={create_clase.id}")
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.json()) == 2
+        assert response.json()[0]['student'] == int(student1.uid)
+        assert response.json()[1]['student'] == int(student2.uid)
+
+
+    def test_by_classroom_with_no_results(self, create_authenticate_user, create_clase):
+        """
+        Test the byClassroom method when no attendances match the given classroom and date.
+        """
+
+        clase = baker.make(Clase, grade='3', level='S', section='A')
+        baker.make(Student, clase=clase)
+        baker.make(Atendance, status='O', kind='I', attendance_type='A', created_by='Manuel Doe')
+
+        response = create_authenticate_user.get(f"/api/atendance/byClassroom/?classroom={create_clase.id}")
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == []
 
     def test_update_atendance_anonymous_user_return_401(self, api_client, create_atendance):
         """Test updating a atendance."""

147e8dca08f5efe64c3d3e593a8c41f8aad73697
podestal <luisrodriguezpodesta@gmail.com>
2025-01-20 11:03:26 -0500

    running scheduled tasks

--- app/amauta/celery.py
+++ app/amauta/celery.py
@@ -1,8 +1,18 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amauta.settings')
 
 celery_app = Celery('amauta')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.conf.timezone = 'America/Lima'
+broker_connection_retry_on_startup = True
 celery_app.autodiscover_tasks()
+
+celery_app.conf.beat_schedule = {
+    'run_task_everyday_at_9am': {
+        'task': 'school.tasks.run_if_valid_day',
+        'schedule': crontab(hour=11, minute=0),
+    }
+}
--- app/school/tasks.py
+++ app/school/tasks.py
@@ -1,8 +1,13 @@
 from celery import shared_task
 from datetime import datetime, date
 from django.core.cache import cache
+from amauta.celery import celery_app
+import holidays
+import pytz
 
 from . import models
+
+PERU_HOLIDAYS = holidays.Peru()
 
 @shared_task
 def mark_absent_students():
@@ -12,6 +17,7 @@ def mark_absent_students():
     for student in students:
         cache_id = f"attendance_{student.uid}_I"
         attendance_in = cache.get(cache_id)
+        print('attendance_in', attendance_in)
         if not attendance_in:
             attendance = models.Atendance.objects.create(
                 student=student,
@@ -30,3 +36,24 @@ def mark_absent_students():
             }
             cache.set(cache_id, cache_data, timeout=54000)
             print(f"Student {student.uid} marked as absent at {datetime.now().isoformat()}")
+
+def should_run_today():
+    """Check if the task should run today."""
+    peru_timezone = pytz.timezone('America/Lima')
+    today = datetime.now(peru_timezone)
+
+    if today.weekday() in [5, 6]:
+        return False
+
+    if today.date() in PERU_HOLIDAYS:
+        return False
+    
+    return True
+
+@shared_task
+def run_if_valid_day():
+    """Run the task if it's a valid day."""
+    if should_run_today():
+        mark_absent_students()
+    else:
+        print("Today is not a valid day to run the task.")

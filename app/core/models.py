7e5fa87080c23fb23caccfb91302800348cf74cb
podestal <luisrodriguezpodesta@gmail.com>
2025-03-15 10:37:12 -0400

    user model update

--- app/core/models.py
+++ app/core/models.py
@@ -4,3 +4,4 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    profile = models.CharField(max_length=255, blank=True, null=True)
--- app/core/serializers.py
+++ app/core/serializers.py
@@ -1,6 +1,9 @@
 from djoser.serializers import UserSerializer as BasedUserSerializer, UserCreateSerializer
 from rest_framework import serializers
 from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
 
 class UserSerializer(BasedUserSerializer):
 
@@ -11,9 +14,41 @@ class UserSerializer(BasedUserSerializer):
     )
 
     class Meta(BasedUserSerializer.Meta):
-        fields = ['id', 'username', 'email', 'groups']
+        fields = ['id', 'username', 'email', 'groups', 'profile']
 
 class CreateUserSerializer(UserCreateSerializer):
-    
+
     class Meta(UserCreateSerializer.Meta):
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password', 'profile', 'first_name', 'last_name']
+
+
+
+    # groups = serializers.ListField(
+    #     child=serializers.CharField(), 
+    #     required=False  
+    # )
+    
+    # class Meta(UserCreateSerializer.Meta):
+    #     fields = ['id', 'username', 'email', 'password', 'groups']
+
+    # def validate_groups(self, value):
+    #     """Ensure that group names exist in the database."""
+    #     groups = Group.objects.filter(name__in=value)
+    #     if len(groups) != len(value):
+    #         missing_groups = set(value) - set(groups.values_list('name', flat=True))
+    #         raise serializers.ValidationError(f"Invalid group names: {missing_groups}")
+    #     return groups  # Return the actual Group queryset
+
+    # def create(self, validated_data):
+    #     print('validated_data', validated_data)
+
+    #     # Pop groups before user creation to prevent direct assignment error
+    #     groups = validated_data.pop('groups', [])
+
+    #     # Create the user
+    #     user = super().create(validated_data)
+
+    #     # Assign groups after user creation
+    #     user.groups.set(groups)
+
+    #     return user

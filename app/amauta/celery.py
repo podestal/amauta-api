1d5771bae3f93a623b226a745b67bf4f05aa1b7c
podestal <luisrodriguezpodesta@gmail.com>
2025-03-17 19:37:21 -0400

    schdule times

--- app/amauta/celery.py
+++ app/amauta/celery.py
@@ -14,10 +14,10 @@ app.autodiscover_tasks(['school'])
 app.conf.beat_schedule = {
     'run_task_everyday_at_9am': {
         'task': 'school.tasks.run_if_valid_day',
-        'schedule': crontab(hour=18, minute=30),
+        'schedule': crontab(hour=10, minute=00),
     },
     'run_task_everyday_at_midnigth': {
         'task': 'school.tasks.mark_on_time_students_out',
-        'schedule': crontab(hour=18, minute=35),
+        'schedule': crontab(hour=16, minute=00),
     }
 }

2b939ebaf79a97c2a1ec6ceaf30f58839763612e
podestal <luisrodriguezpodesta@gmail.com>
2025-03-17 15:25:29 -0400

    celery beat updated

--- app/amauta/celery.py
+++ app/amauta/celery.py
@@ -4,14 +4,14 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amauta.settings')
 
-celery_app = Celery('amauta')
-celery_app.config_from_object('django.conf:settings', namespace='CELERY')
-celery_app.conf.timezone = 'America/Lima'
+app = Celery('amauta')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.timezone = 'America/Lima'
 broker_connection_retry_on_startup = True
 # celery_app.autodiscover_tasks()
-celery_app.autodiscover_tasks(['school'])
+app.autodiscover_tasks(['school'])
 
-celery_app.conf.beat_schedule = {
+app.conf.beat_schedule = {
     'run_task_everyday_at_9am': {
         'task': 'school.tasks.run_if_valid_day',
         'schedule': crontab(hour=19, minute=7),
--- app/school/tasks.py
+++ app/school/tasks.py
@@ -1,7 +1,7 @@
 from celery import shared_task
 from datetime import datetime, date
 from django.core.cache import cache
-from amauta.celery import celery_app
+from amauta.celery import app
 import holidays
 import pytz
 

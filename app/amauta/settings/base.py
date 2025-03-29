6e4eedecf3ab37937f42b9e4edb84677e1398677
podestal <luisrodriguezpodesta@gmail.com>
2025-03-29 07:29:44 -0400

    nginx implemented

--- app/amauta/settings/base.py
+++ app/amauta/settings/base.py
@@ -129,8 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+# STATIC_URL = "/static/"
+
+STATIC_ROOT = "/vol/web/static/"
 STATIC_URL = "/static/"
 
 # Default primary key field type
